2e2d5e4eb6693747e195779e1df383678b2f418e
AndrewMichalik <AndrewM@AndrewM.com>
2016-05-04 08:28:05 +0800

    init

--- searchtosql.go
+++ searchtosql.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
+	fmt.Print("Enter infix string: ")
+	infixString, err := ReadFromInput()
+
+	if err != nil {
+		fmt.Println("Cannot read input: ", err.Error())
+		return
+	}
+
+	fmt.Println("Postfix string: ", infixToPostfix(infixString))
+	return
 
 }
