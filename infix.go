fc14d29ba46aa32512f8f803b32d950fd5a32ed0
AndrewMichalik <AndrewM@AndrewM.com>
2016-05-04 10:22:00 +0800

    init

--- infix.go
+++ infix.go
@@ -1,7 +1,7 @@
 // Package infix contains utility functions for converting Infix to other formats.
 package infix
 
-// ToPostFix returns its argument string in postfix notation.
+// ToPostFix converts an infix argument string into postfix notation.
 func ToPostFix(s string) string {
 	r := []rune(s)
 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
